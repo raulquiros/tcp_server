cbc55893aabddeb25fab597dd1426a00acebc920
raul.quiros <raul.quiros@promocionesfarma.com>
2021-06-15 23:44:35 +0200

    added sku mock repository

--- internal/sku/sku_repository.go
+++ internal/sku/sku_repository.go
@@ -6,3 +6,5 @@ type SkuRepository interface {
 	FindAll(ctx context.Context) ([]string, error)
 	Save(ctx context.Context, sku string) error
 }
+
+//go:generate mockery --case=snake --outpkg=storagemocks --output=../platform/storage/storagemocks --name=SkuRepository
